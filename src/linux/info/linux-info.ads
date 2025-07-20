896e43f036f1dea04757b6f5c1ac1ac0c2bc04d5
naolos-regen <naolos.regen@proton.me>
2025-07-20 21:44:23 +0200

    more info's lulge, no body yet

--- src/linux/info/linux-info.ads
+++ src/linux/info/linux-info.ads
@@ -12,9 +12,10 @@ package Linux.Info is
    subtype Detailed_Percentage is Float   range 0.0 .. 100.0;
 
    type Information is record
-      Battery_Information   : Battery_Pointer     := Get_Battery_Information;;
-      System_Information    : System_Info_Pointer := Get_System_Information;
-      Disk_Information      : Statfs_Pointer      := Get_Statfs_Information;
+      Battery_Information            : Battery_Pointer                  := Get_Battery_Information;
+      System_Information             : System_Info_Pointer              := Get_System_Information;
+      Disk_Information               : Statfs_Pointer                   := Get_Statfs_Information;
+      Operating_System_Information   : Operating_System_Release_Pointer := Get_Operating_System_Release_Information;
    end record;
 
    type Information_Pointer is access all Information;
@@ -52,19 +53,19 @@ package Linux.Info is
 
    package Linux.Info.System is
       type System_Info is record
-         Uptime            : Interfaces.C.long;
-         Loads             : array (0 .. 2) of Interfaces.C.unsigned_long; 
-         Total_Ram,        : Interfaces.C.unsigned_long;
-         Free_Ram,         : Interfaces.C.unsigned_long;
-         Shared_Ram,       : Interfaces.C.unsigned_long;
-         Buffer_Ram,       : Interfaces.C.unsigned_long;
-         Total_Swap,       : Interfaces.C.unsigned_long;
-         Free_Swap         : Interfaces.C.unsigned_long;
-         Procs             : Interfaces.C.unsigned_short;
-         Total_High        : Interfaces.C.unsigned_long;
-         Free_High         : Interfaces.C.unsigned_long;
-         Mem_Unit          : Interfaces.C.unsigned_long;
-         Whatever          : array (0 .. 7) of Interfaces.c.char;
+            Uptime            : constant Interfaces.C.long;
+            Loads             : constant array (0 .. 2) of Interfaces.C.unsigned_long; 
+            Total_Ram,        : constant Interfaces.C.unsigned_long;
+            Free_Ram,         : constant Interfaces.C.unsigned_long;
+            Shared_Ram,       : constant Interfaces.C.unsigned_long;
+            Buffer_Ram,       : constant Interfaces.C.unsigned_long;
+            Total_Swap,       : constant Interfaces.C.unsigned_long;
+            Free_Swap         : constant Interfaces.C.unsigned_long;
+            Procs             : constant Interfaces.C.unsigned_short;
+            Total_High        : constant Interfaces.C.unsigned_long;
+            Free_High         : constant Interfaces.C.unsigned_long;
+            Mem_Unit          : constant Interfaces.C.unsigned_long;
+            Whatever          : constant array (0 .. 7) of Interfaces.c.char;
       end record;
       pragma Convention (C, Sysinfo);
 
@@ -76,7 +77,7 @@ package Linux.Info is
       function Get_System_Information return System_Info_Pointer;
 
    end Linux.Info.System;
-
+   
    package Linux.Info.Bios is
 
    end Linux.Info.Bios;
@@ -91,17 +92,17 @@ package Linux.Info is
 
    package Linux.Info.Disk is
       type Statfs is record 
-         F_Type      : C.long;
-         F_Bsize     : C.long;
-         F_Blocks    : C.unsigned_long;
-         F_Bfree     : C.unsigned_long;
-         F_Bavail    : C.unsigned_long;
-         F_Files     : C.unsigned_long;
-         F_Ffree     : C.unsigned_long;
-         F_Fsid      : C.long;
-         F_NameLen   : C.long;
-         F_Frsize    : C.long;
-         F_Spare     : array (0 .. 5) of C.long;
+         F_Type      : constant C.long;
+         F_Bsize     : constant C.long;
+         F_Blocks    : constant C.unsigned_long;
+         F_Bfree     : constant C.unsigned_long;
+         F_Bavail    : constant C.unsigned_long;
+         F_Files     : constant C.unsigned_long;
+         F_Ffree     : constant C.unsigned_long;
+         F_Fsid      : constant C.long;
+         F_NameLen   : constant C.long;
+         F_Frsize    : constant C.long;
+         F_Spare     : constant array (0 .. 5) of C.long;
       end record;
       pragma Convention (C, Statfs);
 
@@ -127,7 +128,24 @@ package Linux.Info is
    end Linux.Info.Monitor_Brightness;
 
    package Linux.Info.Operating_System is
+      type Operating_System_Release is record
+         Name                 : constant String;
+         Pretty Name          : constant String;
+         ID                   : constant String;
+         Build_ID             : constant String;
+         ANSI_Color           : constant String;
+         Home_URL             : constant String; -- maybe create URL type (?)
+         Documentation_URL    : constant String; -- maybe create URL type (?)
+         Support_URL          : constant String; -- maybe create URL type (?)
+         Bug_Report_URL       : constant String; -- maybe create URL type (?)
+         Privacy_Policy_URL   : constant String; -- maybe create URL type (?)
+         Logo                 : constant String;
+      end record; -- other Idea is to interface with C fopen, but ada uses the same but more reliable and safe
 
+      type Operating_System_Release_Pointer is access all Operating_System_Release;
+
+      function Get_Operating_System_Release_Information return Operating_System_Release_Pointer;
+      
    end Linux.Info.Operating_System;
 
 end Linux.Info;
