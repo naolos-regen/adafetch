fdca421e3b4756ad9016e19f8e095e380fd93b51
naolos-regen <naolos.regen@proton.me>
2025-07-23 21:12:40 +0200

    bios

--- src/linux/info/linux-info.ads
+++ src/linux/info/linux-info.ads
@@ -13,6 +13,7 @@ package Linux.Info is
 
    type Information is record
       Battery_Information            : Battery_Pointer                   := Get_Battery_Information;
+      Bios_Information               : Bios_Information_Pointer          := Get_Bios_Information;
       System_Information             : System_Info_Pointer               := Get_System_Information;
       Disk_Information               : Statfs_Pointer                    := Get_Statfs_Information;
       Operating_System_Information   : Operating_System_Release_Pointer  := Get_Operating_System_Release_Information;
@@ -79,6 +80,46 @@ package Linux.Info is
    end Linux.Info.System;
    
    package Linux.Info.Bios is
+      type Bios is record
+         Date    : constant String; -- bd
+         Release : constant String; -- br
+         Vendor  : constant String; -- bvn:
+         Version : constant String; -- bvr:
+      end record;
+
+      type System is record
+         Vendor  : constant String; -- svn
+      end record;
+
+      type Board is record
+         Name      : constant String; -- rn
+         Vendor    : constant String; -- rvn
+         Version   : constant String; -- rvr
+      end record;
+
+      type Chassis is record
+         Name      : constant String; -- ( Name <=> Type ) ct
+         Vendor    : constant String; -- cvn:
+         Version   : constant String; -- cvr:
+      end record;
+
+      type Product is record
+         Name      : constant String; -- pn:
+         SKU       : constant String; -- sku:
+         Version   : constant String; -- pvr:
+      end record;
+
+      type Bios_Info is record
+         Bios_Info    : Bios;
+         Board_Info   : Board;
+         Chassis_Info : Chassis;
+         Product_Info : Product;
+         System_Info  : System;
+      end record;
+
+      type Bios_Information_Pointer is access all Bios_Info;
+
+      function Get_Bios_Information return Bios_Information_Pointer;
 
    end Linux.Info.Bios;
 
@@ -150,7 +191,10 @@ package Linux.Info is
          Bug_Report_URL       : constant String; -- maybe create URL type (?)
          Privacy_Policy_URL   : constant String; -- maybe create URL type (?)
          Logo                 : constant String;
-      end record; -- other Idea is to interface with C fopen, but ada uses the same but more reliable and safe
+      end record; 
+                  -- other Idea is to interface with C fopen, 
+                  -- but ada uses the same but more reliable and safe,
+                  -- via the use of lightweight exceptions
 
       type Operating_System_Release_Pointer is access all Operating_System_Release;
 
