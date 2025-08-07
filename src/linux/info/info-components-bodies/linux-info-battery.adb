3661fe1956a1a4cc53f8fc5609f5b2fc8479ab44
naolos-regen <naolos.regen@proton.me>
2025-08-07 17:17:46 +0200

    it worky but not parsy

--- src/linux/info/info-components-bodies/linux-info-battery.adb
+++ src/linux/info/info-components-bodies/linux-info-battery.adb
@@ -1,29 +1,51 @@
 package body Linux.Info.Battery is 
-   package Battery_Vec is new Ada.Containers.Vectors (
-      Index_Type => Integer,
-      Element_Type => String);
+
+   package Vec is new Vectors (Natural, Unbounded_String);
    
-   use type Battery_Vec.Cursor;
+   use Vec;
+
+   Battery_Path : constant String := "/sys/class/power_supply/";
+
+   type Battery is record
+      Dev_Type           : Unbounded_String;
+      Name               : Unbounded_String;
+      Status             : Unbounded_String;
+      Model_Name         : Unbounded_String;
+      Manufacturer       : Unbounded_String;
+      Technology         : Unbounded_String;
+      Present            : Boolean;
+      Cycle_Count        : Integer;
+      Voltage_Min_Design : Integer;
+      Voltage_Now        : Integer;
+      Power_Now          : Integer;
+      Energy_Full_Design : Integer;
+      Energy_Full        : Integer;
+      Energy_Now         : Integer;
+      Capacity           : Integer;
+      Capacity_Level     : Unbounded_String;
+   end record;
+
+   type Battery_Pointer is access all Battery;
    
    Battery_Path_Error : exception;
-   
+
    function Find_Path return String is
-      Default_Battery_Path : constant String := Linux.Info.Battery_Path;
-      Default_Result       : constant String := "not fonud";
-      Battery_Entry        : Directory_Entry_Type;
-      Battery_Search       : Search_Type;
-      Battery_Filter       : Filter_Type := (Directory => True, others => False);
+      Default_Result : constant String := "not found";
+      Battery_Entry  : Directory_Entry_Type;
+      Battery_Search : Search_Type;
+      Battery_Filter : constant Filter_Type 
+                     := (Directory => True, others => False);
    begin
-      if not Exists (Default_Battery_Path) then
-         raise Battery_Path_Error with "Directory Does Not Exist" & Default_Battery_Path;
+      if not Exists (Battery_Path) then
+         raise Battery_Path_Error with 
+         "Directory doesn't exist" & Battery_Path;
       end if;
 
       Start_Search (
-                    Battery_Search, 
-                    Directory => Default_Battery_Path,
-                    Pattern => "BAT*",
-                    Filter  => Battery_Filter
-                    );
+                     Battery_Search,
+                     Directory => Battery_Path,
+                     Pattern => "BAT*",
+                     Filter => Battery_Filter);
 
       while More_Entries (Battery_Search) loop
          Get_Next_Entry (Battery_Search, Battery_Entry);
@@ -37,8 +59,7 @@ package body Linux.Info.Battery is
       return Default_Result;
    end Find_Path;
    
-   function Parse_Lines (Vector : in out Vector) return Battery_Pointer is
-      Battery_Map  : Empty_Map;
+   function Parse_Lines (Vec: in out Vector) return Battery_Pointer is
       Battery_Data : Battery := (
          Dev_Type           => To_Unbounded_String(""),
          Name               => To_Unbounded_String(""),
@@ -56,82 +77,48 @@ package body Linux.Info.Battery is
          Energy_Now         => 0,
          Capacity           => 0,
          Capacity_Level     => To_Unbounded_String("") 
-      );
+         );
    begin
-      for Line of Vector loop
+      for Line of Vec loop
          declare
             Equal_Pos : constant Natural := Index (Line, "=");
          begin
-            if Equal_Pos = 0 then
-               continue;
+            if Equal_Pos /= 0 then
+               declare
+                  Key   : String := Slice (Line, 1, Equal_Pos - 1);
+                  Value : String := Slice (Line, Equal_Pos + 1, Length (Line));
+               begin
+                  Put_Line (Key & " is " & Value);
+               end;
             end if;
-
-            declare
-               Key   : constant String := Line (Line'First .. Equal_Pos - 1);
-               Value : constant String := Line (Equal_Pos + 1 .. Line'Last);
-            begin
-               if Key = "POWER_SUPPLY_NAME" then
-                  Battery_Data.Name := To_Unbounded_String(Value);
-               elsif Key = "DEVTYPE" then
-                  Battery_Data.Dev_Type := To_Unbounded_String(Value);
-               elsif Key = "POWER_SUPPLY_STATUS" then
-                  Battery_Data.Status := To_Unbounded_String(Value);
-               elsif Key = "POWER_SUPPLY_RESENT" then
-                  if Value "1" then
-                     Battery_Data.Present := True;
-                  end if;
-               elsif Key = "POWER_SUPPLY_TECHNOLOGY" then
-                  Battery_Data.Technology := To_Unbounded_String(Value);
-               elsif Key = "POWER_SUPPLY_CYCLE_COUNT" then
-                  Battery_Data.Cycle_Count := Integer'Value (Value);
-               elsif Key = "POWER_SUPPLY_VOLTAGE_NOW" then
-                  Battery_Data.Voltage_Now := Integer'Value (Value);
-               elsif Key = "POWER_SUPPLY_VOLTAGE_MIN_DESIGN" then
-                  Battery_Data.Voltage_Min_Design := Integer'Value (Value);
-               elsif Key = "POWER_SUPPLY_POWER_NOW" then
-                  Battery_Data.Power_Now := Integer'Value (Value);
-               elsif Key = "POWER_SUPPLY_ENERGY_FULL_DESIGN" then
-                  Battery_Data.Energy_Full_Design := Integer'Value (Value);
-               elsif Key = "POWER_SUPPLY_ENERGY_FULL" then 
-                  Battery_Data.Energy_Full = Integer'Value (Value);
-               elsif Key = "POWER_SUPPLY_ENERGY_NOW" then
-                  Battery_Data.Energy_Now = Integer'Value (Value);
-               elsif Key = "POWER_SUPPLY_CAPACITY" then
-                  Battery_Data.Capacity = Percentage'Value (Value);
-               elsif Key = "POWER_SUPPLY_CAPACITY_LEVEL" then
-                  Battery_Data.Capacity_Level := To_Unbounded_String(Value);
-               elsif Key = "POWER_SUPPLY_MANUFACTUTER" then 
-                  Battery_Data.Manufacturer := To_Unbounded_String(Value);
-               end if;
-            end;
          end;
       end loop;
-
       return new Battery'(Battery_Data);
    end Parse_Lines;
 
    function Get_Battery_Information return Battery_Pointer is
-      Result_Battery_Path : constant String := Find_Path & "/uevent";
-      File                : File_Type;
-      Battery_Result      : Battery_Pointer;
-      Battery_Vector      : Empty_Vector;
+      Result   : constant String := Find_Path & "/uevent";
+      File     : File_Type;
+      B_Result : Battery_Pointer;
+      B_Vector : Vector;
    begin
-      if Battery_Path = "not found" then
-         raise Battery_Path_Error with "Not Found";
+      if Result = "not found" then
+         raise Battery_Path_Error with Result;
       end if;
-      
-      Open (File, In_File, Result_Battery_Path);
 
-      while not End_Of_File loop
+      Open (File, In_File, Result);
+
+      while not End_Of_File (File) loop
          declare
-            Line : constant String := Get_Line;
+            Line : constant String := Get_Line (File);
          begin
-            Append (Battery_Vector, Line);
+            Append (B_Vector, To_Unbounded_String(Line));
          end;
       end loop;
+
       Close (File);
-      
-      return Parse_Lines (Battery_Vector);
+
+      return Parse_Lines (B_Vector);
    end Get_Battery_Information;
 
 end Linux.Info.Battery;
