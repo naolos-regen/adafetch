58436beb9d836b622992f8a9c774c3faa2114662
naolos-regen <naolos.regen@proton.me>
2025-08-25 06:23:39 +0200

    fix

--- src/linux/info/info-components-bodies/linux-info-cpu_useage.adb
+++ src/linux/info/info-components-bodies/linux-info-cpu_useage.adb
@@ -1,28 +1,49 @@
-with Ada.Directories; use Ada.Directories;
-with Ada.Text_IO;     use Ada.Text_IO;
-
 package body Linux.Info.Cpu_Useage is 
  
    CPU_Useage_Path_Error : exception;
+   
+   procedure Split_Vector (V : in out Vector) is 
+      Subs  : Slice_Set;
+      Seps  : constant String := " " & Latin_1.HT;
+   begin
+      for I of V loop
+         Create (Subs, To_String (I), Seps, Multiple);
+         declare
+            V2 : Vector := Empty_Vector;
+         begin
+            for J in 2 .. Slice_Count (Subs) loop
+               declare
+                  Sub : constant String := Slice (Subs, J);
+               begin
+                  Append (V2, Sub);
+               end;
+            end loop;
+         end;
+      end loop;
+   end Split_Vector;
 
    function Get_Cpu_Percentage return Percentage is
       Default_Cpu_Useage_Path : constant String := CPU_Useage_Path;
-      File : File_Type;
+      File                    : File_Type;
+      C_Vec                   : Vector := Empty_Vector;
    begin
       if not Exists (Default_Cpu_Useage_Path) then
          raise CPU_Useage_Path_Error with "File Does Not Exist" & Default_CPU_Useage_Path;
       end if;
       
       Open (File, In_File, Default_Cpu_Useage_Path);
-      
-      while End_Of_File (File) loop
+      Set_Line_Length (File, 7);
+
+      while not End_Of_File (File) loop
          declare
             Line : constant String := Get_Line(File);
          begin
-            -- TODO: Schemantics here
+            Append (C_Vec, To_Unbounded_String (Line));
          end;
       end loop;
       
+      Split_Vector (C_Vec);
+
    end Get_Cpu_Percentage;
 
 end Linux.Info.Cpu_Useage;
--- src/linux/info/info-components-structures/linux-info-cpu_useage.ads
+++ src/linux/info/info-components-structures/linux-info-cpu_useage.ads
@@ -1,17 +1,26 @@
+with Ada.Text_IO;            use Ada.Text_IO;
+with Ada.Directories;        use Ada.Directories;
+with Ada.Containers;         use Ada.Containers;
+with Ada.Strings.Unbounded;  use Ada.Strings.Unbounded;
+with Ada.Characters.Latin_1; use Ada.Characters;
+
 package Linux.Info.Cpu_Useage is
-   CPU_Path : constant String := "/proc/cpuinfo";
+   CPU_Path : constant String := "/proc/stat";
+
+   package Vec is new Vector (Natural, Unbounded_String);
+   use Vec;
 
    type Cpu_Stats is record
-         User        : constant Integer;
-         Nice        : constant Integer;
-         System      : constant Integer;
-         Idle        : constant Integer;
-         Io_Wait     : constant Integer;
-         Irq         : constant Integer;
-         Soft_Irq    : constant Integer;
-         Steal       : constant Integer;
-         Guest       : constant Integer;
-         Guest_Nice  : constant Integer;
+         User        : Integer;
+         Nice        : Integer;
+         System      : Integer;
+         Idle        : Integer;
+         Io_Wait     : Integer;
+         Irq         : Integer;
+         Soft_Irq    : Integer;
+         Steal       : Integer;
+         Guest       : Integer;
+         Guest_Nice  : Integer;
    end record;
    
    function Get_Cpu_Percentage return Percentage;
